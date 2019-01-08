dbe4b2a03e4c2e07ae71045e84b08b19a59dac0e
Troels Irgens-Møller <troelsirgensmoeller@gmail.com>
2019-01-08 15:34:27 +0100

    Initial

---
+++ lib/ecs_cluster.ex
@@ -0,0 +1,18 @@
+defmodule EcsCluster do
+  @moduledoc """
+  Documentation for EcsCluster.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EcsCluster.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecs_cluster_test.exs
@@ -0,0 +1,8 @@
+defmodule EcsClusterTest do
+  use ExUnit.Case
+  doctest EcsCluster
+
+  test "greets the world" do
+    assert EcsCluster.hello() == :world
+  end
+end
